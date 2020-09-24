d02054a7dba243cd15df7ceda5cc21111637de56
Anwesha <jazzann24@gmail.com>
2020-09-24 13:15:30 +0530

    styling.

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bdf7c0b5b8c22f90cd01";
+/******/ 	var hotCurrentHash = "d0e6afd37d13e40967ae";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
