5a6156f1eb32a0bd64b6ade94b8d758c820dbc18
Anwesha <jazzann24@gmail.com>
2020-09-26 16:45:54 +0530

    Avatrs added.

--- components/AnsAlert.js
+++ components/AnsAlert.js
@@ -1,29 +1,3 @@
-import Swal from 'sweetalert2';
-
-export default (isCorrect) => {
-    
-    if (isCorrect === 1)
-    {
-        let timerInterval;
-        Swal.fire({
-            padding: "60px",
-            position: 'center',
-            icon: 'success',
-            type: 'success',
-            title: 'You gave the correct answer',
-            showConfirmButton: false,
-            timer: 2000,
-            timerProgressBar: true,
-            willOpen: () => {
-                Swal.showLoading()
-                timerInterval = setInterval(() => {
-                
-                }, 100)
-            },
-            onClose: () => {
-                clearInterval(timerInterval)
-            }
-            })
 
 import Swal from 'sweetalert2';
 
@@ -53,61 +27,6 @@ export default (isCorrect) => {
             })
 
 
-    }
-            if (isCorrect === 0)
-            {   let timerInterval
-                Swal.fire({
-                    padding: "60px",
-                    position: 'center',
-                    icon: 'error',
-                    title: 'Oops! Wrong answer.',
-                    text: 'Try again',
-                    showConfirmButton: false,
-                    timer: 2000,
-                    timerProgressBar: true,
-                    willOpen: () => {
-                        Swal.showLoading()
-                        timerInterval = setInterval(() => {
-                        
-                        }, 100)
-                    },
-                    onClose: () => {
-                        clearInterval(timerInterval)
-                    }  
-        
-
-            })
-    }
-        if (isCorrect === 9)
-        {
-            
-            Swal.fire({
-                padding: "60px",
-                position: 'center',
-                icon: 'error',
-                title: 'Times up!',
-                text: 'Ahaaa sorry!',
-                showConfirmButton: false,
-                
-               
-
-        })
-    }
-        
-        if (isCorrect === 8)
-        Swal.fire({
-            padding: "60px",
-            position: 'center',
-            icon: 'info',
-            title: 'Error !',
-            text: 'Aha prohibited ! ;_;',
-            showConfirmButton: false
-
-        })
-
-
-
-}
     }
             if (isCorrect === 0)
             {   let timerInterval
